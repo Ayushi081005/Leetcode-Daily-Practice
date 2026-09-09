8595473079982723223f48335579dda0a05530e3
Ayushi <ayushishrivastava2005@gmail.com>
2026-09-09 15:45:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (33.42%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,68 +15,13 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            List<Integer> levelValues = new ArrayList<>();
-            boolean hasNonNullInNextLevel = false;
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode node = queue.poll();
-
-                if (node == null) {
-                    levelValues.add(null);
-                } else {
-                    levelValues.add(node.val);
-                    
-                    queue.add(node.left);
-                    queue.add(node.right);
-
-                    if (node.left != null || node.right != null) {
-                        hasNonNullInNextLevel = true;
-                    }
-                }
-            }
-
-            if (!isPalindrome(levelValues)) {
-                return false;
-            }
-
-            if (!hasNonNullInNextLevel) {
-                break;
-            }
-        }
-
-        return true;
+        return root == null || isMirror(root.left, root.right);
     }
 
-    private boolean isPalindrome(List<Integer> list) {
-        int left = 0;
-        int right = list.size() - 1;
-
-        while (left < right) {
-            Integer val1 = list.get(left);
-            Integer val2 = list.get(right);
-
-            if (val1 == null && val2 == null) {
-                left++;
-                right--;
-                continue;
-            }
-            if (val1 == null || val2 == null || !val1.equals(val2)) {
-                return false;
-            }
-
-            left++;
-            right--;
-        }
-
-        return true;
-
-        
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null || t2 == null) return t1 == t2;
+        return t1.val == t2.val 
+            && isMirror(t1.left, t2.right) 
+            && isMirror(t1.right, t2.left);
     }
 }
