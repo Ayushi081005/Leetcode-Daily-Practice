1f07e98adb17a527466beda1dbb56ba011f0dba9
Ayushi <ayushishrivastava2005@gmail.com>
2026-09-10 12:17:59 +0530

    Time: 2560 ms (5.00%) | Memory: 41.9 MB (68.35%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,15 +1,19 @@
 class Solution {
     public int numTrees(int n) {
-        int[] uniqueBST = new int[n + 1];
-        uniqueBST[0] = 1;
-        uniqueBST[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                uniqueBST[i] += uniqueBST[j - 1] * uniqueBST[i - j];
-            }
+        return countBST(1, n);
+    }
+    
+    private int countBST(int start, int end) {
+        if (start >= end) {
+            return 1;
         }
-
-        return uniqueBST[n];
+        
+        int total = 0;
+        for (int i = start; i <= end; i++) {
+            int left = countBST(start, i - 1);
+            int right = countBST(i + 1, end);
+            total += left * right;
+        }
+    return total;
     }
 }
